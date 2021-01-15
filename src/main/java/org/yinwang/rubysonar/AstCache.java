a6c07c9c8a317239ed6258d875b6b232b5d0c56e
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-01-15 20:30:02 +0000

    Day 3

--- src/main/java/org/yinwang/rubysonar/AstCache.java
+++ src/main/java/org/yinwang/rubysonar/AstCache.java
@@ -111,4 +111,7 @@ public class AstCache {
 
     /**
      * Each source file's AST is saved in an object file named for the MD5
-     * checksum of the source file.  All that is 
+     * checksum of the source file.  All that is needed is the MD5, but the
+     * file's base name is included for ease of debugging.
+     */
+  
