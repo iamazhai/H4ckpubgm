7688474366fe7088a72186452d1f364734847068
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-03-06 12:19:34 +0000

    import zuul job settings from project-config

--- src/main/java/org/yinwang/rubysonar/Constants.java
+++ src/main/java/org/yinwang/rubysonar/Constants.java
@@ -3,4 +3,6 @@ package org.yinwang.rubysonar;
 public class Constants {
     public static final String SELFNAME = "self";
     public static final String INSTNAME = "#this";
-    public st
+    public static final String IDSEP = "^";
+    public static final String IDSEP_REGEX = "\\^";
+}
