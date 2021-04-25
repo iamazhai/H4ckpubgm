6651f78e58c68adfdc8dfdb0225e87ef7c70e952
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-04-25 13:09:47 +0000

    add issue templates

--- src/main/java/org/yinwang/rubysonar/Parser.java
+++ src/main/java/org/yinwang/rubysonar/Parser.java
@@ -142,4 +142,6 @@ public class Parser {
                     for (Node node : posKey) {
                         if (node instanceof Assign && ((Assign) node).target instanceof Name) {
                             kws.add(new Keyword(((Name) ((Assign) node).target).id,
-                                    ((
+                                    ((Assign) node).value,
+                                    file,
+                                  
