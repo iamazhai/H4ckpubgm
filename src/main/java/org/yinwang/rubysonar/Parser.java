bd94283da87d1d671d85d619771b23f57933a529
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-08-12 07:39:46 +0000

    [maven-release-plugin][skip ci]prepare for next development iteration

--- src/main/java/org/yinwang/rubysonar/Parser.java
+++ src/main/java/org/yinwang/rubysonar/Parser.java
@@ -503,4 +503,8 @@ public class Parser {
             return Op.Match;
         }
 
-        if (n
+        if (name.equals("!~")) {
+            return Op.NotMatch;
+        }
+
+        if (name.equals("==") || name.equals("===")) {
