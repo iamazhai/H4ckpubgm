b13f26d3d623a5699ff4023459d2d7e7a099c6f5
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-10-25 09:42:07 +0000

    Fix a couple typos

--- src/main/java/org/yinwang/rubysonar/Test.java
+++ src/main/java/org/yinwang/rubysonar/Test.java
@@ -48,3 +48,103 @@ public class Test {
 
 
     public void generateRefs() {
+
+        List<Map<String, Object>> refs = new ArrayList<>();
+        for (Map.Entry<Node, List<Binding>> e : analyzer.references.entrySet()) {
+
+            String file = e.getKey().file;
+
+            // only record those in the inputDir
+            if (file != null && file.startsWith(Analyzer.self.projectDir)) {
+                file = _.projRelPath(file);
+                Map<String, Object> writeout = new LinkedHashMap<>();
+
+                Map<String, Object> ref = new LinkedHashMap<>();
+                ref.put("name", e.getKey().name);
+                ref.put("file", file);
+                ref.put("start", e.getKey().start);
+                ref.put("end", e.getKey().end);
+
+                List<Map<String, Object>> dests = new ArrayList<>();
+                for (Binding b : e.getValue()) {
+                    String destFile = b.file;
+                    if (destFile != null && destFile.startsWith(Analyzer.self.projectDir)) {
+                        destFile = _.projRelPath(destFile);
+                        Map<String, Object> dest = new LinkedHashMap<>();
+                        dest.put("name", b.node.name);
+                        dest.put("file", destFile);
+                        dest.put("start", b.start);
+                        dest.put("end", b.end);
+                        dests.add(dest);
+                    }
+                }
+                if (!dests.isEmpty()) {
+                    writeout.put("ref", ref);
+                    writeout.put("dests", dests);
+                    refs.add(writeout);
+                }
+            }
+        }
+
+        String json = gson.toJson(refs);
+        _.writeFile(expecteRefsFile, json);
+    }
+
+
+    public boolean checkRefs() {
+        List<Map<String, Object>> failedRefs = new ArrayList<>();
+        List<Map<String, Object>> extraRefs = new ArrayList<>();
+        String json = _.readFile(expecteRefsFile);
+        if (json == null) {
+            _.msg("Expected refs not found in: " + expecteRefsFile +
+                    "Please run Test with -exp to generate");
+            return false;
+        }
+        List<Map<String, Object>> expectedRefs = gson.fromJson(json, List.class);
+        for (Map<String, Object> r : expectedRefs) {
+            Map<String, Object> refMap = (Map<String, Object>) r.get("ref");
+            Dummy dummy = makeDummy(refMap);
+
+            List<Map<String, Object>> dests = (List<Map<String, Object>>) r.get("dests");
+            List<Binding> actualDests = analyzer.references.get(dummy);
+            List<Map<String, Object>> failedDests = new ArrayList<>();
+            List<Map<String, Object>> extraDests = new ArrayList<>();
+
+            for (Map<String, Object> d : dests) {
+                // names are ignored, they are only for human readers
+                String file = _.projAbsPath((String) d.get("file"));
+                int start = (int) Math.floor((double) d.get("start"));
+                int end = (int) Math.floor((double) d.get("end"));
+
+                if (actualDests == null ||
+                        !checkBindingExist(actualDests, file, start, end))
+                {
+                    failedDests.add(d);
+                }
+            }
+
+            if (actualDests != null && !actualDests.isEmpty()) {
+                for (Binding b : actualDests) {
+                    String destFile = b.file;
+                    if (destFile != null && destFile.startsWith(Analyzer.self.projectDir)) {
+                        destFile = _.projRelPath(destFile);
+                        Map<String, Object> d1 = new LinkedHashMap<>();
+                        d1.put("file", destFile);
+                        d1.put("start", b.start);
+                        d1.put("end", b.end);
+                        extraDests.add(d1);
+                    }
+                }
+            }
+
+            // record the ref & failed dests if any
+            if (!failedDests.isEmpty()) {
+                Map<String, Object> failedRef = new LinkedHashMap<>();
+                failedRef.put("ref", refMap);
+                failedRef.put("dests", failedDests);
+                failedRefs.add(failedRef);
+            }
+
+            if (!extraDests.isEmpty()) {
+                Map<String, Object> extraRef = new LinkedHashMap<>();
+                extraRef.put("ref", refMap);
