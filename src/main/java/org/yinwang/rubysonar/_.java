33e43120ad52f8908b54b3c557364f108484b4bf
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-11-16 13:49:18 +0000

    See Changelog.

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -35,3 +35,8 @@ public class _ {
 
     public static boolean same(@Nullable Object o1, @Nullable Object o2) {
         if (o1 == null) {
+            return o2 == null;
+        } else {
+            return o1.equals(o2);
+        }
+    }
