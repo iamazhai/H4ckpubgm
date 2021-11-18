808b261ccc1c18dd8778c4551014cb1d4275274a
iamazhai <87902280+iamazhai@users.noreply.github.com>
2021-11-18 13:39:06 +0000

    Added Catalan (Valencian) translation

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -44,4 +44,8 @@ public class _ {
 
     public static String getSystemTempDir() {
         String tmp = System.getProperty("java.io.tmpdir");
-    
+        String sep = System.getProperty("file.separator");
+        if (tmp.endsWith(sep)) {
+            return tmp;
+        }
+  
