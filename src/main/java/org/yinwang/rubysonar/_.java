25ca29f64e7ebb30b9577b0772255ab556c468d0
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-01-14 19:23:32 +0000

    version 0.0.15

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -328,4 +328,8 @@ public class _ {
     public static String formatTime(long millis) {
         long sec = millis / 1000;
         long min = sec / 60;
-        sec = 
+        sec = sec % 60;
+        long hr = min / 60;
+        min = min % 60;
+
+        return hr + ":" + min + ":" + 
