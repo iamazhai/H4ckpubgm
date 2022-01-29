faf1751fee32a204b77e2afe0e4d2dcf09cc1065
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-01-29 17:50:26 +0000

    bumped assembly versions

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -377,4 +377,8 @@ public class _ {
 
 
     public static File makePath(String... files) {
-        File ret = new
+        File ret = new File(files[0]);
+
+        for (int i = 1; i < files.length; i++) {
+            ret = new File(ret, files[i]);
+  
