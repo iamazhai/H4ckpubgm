d45224190d3ba08523b7c1e4815ab27bfbab9de1
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-02-21 18:34:06 +0000

    removed keys

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -443,4 +443,11 @@ public class _ {
 
     public static String projRelPath(String file) {
         if (file.startsWith(Analyzer.self.projectDir)) {
-            return file.substring(Analyzer.self.projectDir.length() +
+            return file.substring(Analyzer.self.projectDir.length() + 1);
+        } else {
+            return file;
+        }
+    }
+
+
+    public static String projAbsPa
