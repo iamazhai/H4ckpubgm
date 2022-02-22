cc4c555c68d49ed415a3e9232387b929ca684ea3
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-02-22 07:08:12 +0000

    Add helpers

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -452,4 +452,6 @@ public class _ {
 
     public static String projAbsPath(String file) {
         if (file.startsWith("/") || file.startsWith(Analyzer.self.projectDir)) {
-            
+            return file;
+        } else {
+            return makePathString(Analyzer.self.projectDi
