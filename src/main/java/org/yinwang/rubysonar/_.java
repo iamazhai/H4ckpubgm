4e215aadb83bd0e6d42064faefc27c4b8f6038a5
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-02-23 16:47:08 +0000

    genirq: Add missing irq_to_desc export for CONFIG_SPARSE_IRQ=n

--- src/main/java/org/yinwang/rubysonar/_.java
+++ src/main/java/org/yinwang/rubysonar/_.java
@@ -468,4 +468,9 @@ public class _ {
     @NotNull
     public static File joinPath(String dir, String file) {
         File file1 = new File(dir);
-    
+        File file2 = new File(file1, file);
+        return file2;
+    }
+
+
+    public static String locateTmp(S
