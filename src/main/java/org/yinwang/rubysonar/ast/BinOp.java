fc1f1eee7f0ec8fbf33fa83e2fbb1edd200e6d4b
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-03-13 21:45:48 +0000

    layers

--- src/main/java/org/yinwang/rubysonar/ast/BinOp.java
+++ src/main/java/org/yinwang/rubysonar/ast/BinOp.java
@@ -2,4 +2,11 @@ package org.yinwang.rubysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.rubysonar.State;
-import org.yinwang.rubyson
+import org.yinwang.rubysonar.types.Type;
+
+
+public class BinOp extends Node {
+
+    @NotNull
+    public Node left;
+    @NotNull
