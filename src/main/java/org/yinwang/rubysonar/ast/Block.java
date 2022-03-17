02b8583bee3a13b07a92b32e803246374e8bb433
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-03-17 13:01:21 +0000

    Refactored unit tests

--- src/main/java/org/yinwang/rubysonar/ast/Block.java
+++ src/main/java/org/yinwang/rubysonar/ast/Block.java
@@ -3,4 +3,10 @@ package org.yinwang.rubysonar.ast;
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.rubysonar.Analyzer;
 import org.yinwang.rubysonar.State;
-import org.yinwang.ruby
+import org.yinwang.rubysonar.types.Type;
+import org.yinwang.rubysonar.types.UnionType;
+
+import java.util.List;
+
+
+public cl
