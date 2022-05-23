3209e5ff3c525066ba07729b53732e00f5654e04
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-05-23 19:32:55 +0000

    upd 1.0.18

--- src/main/java/org/yinwang/rubysonar/ast/Index.java
+++ src/main/java/org/yinwang/rubysonar/ast/Index.java
@@ -2,4 +2,12 @@ package org.yinwang.rubysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.rubysonar.State;
-import org.yinwang.rubysonar.types.Typ
+import org.yinwang.rubysonar.types.Type;
+
+
+public class Index extends Node {
+
+    public Node value;
+
+
+    public Index(Node n, Stri
