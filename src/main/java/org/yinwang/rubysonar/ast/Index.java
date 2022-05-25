02519a0cb634f1ac4dabd74256c851e3f9d50b75
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-05-25 05:40:56 +0000

    bumped revs

--- src/main/java/org/yinwang/rubysonar/ast/Index.java
+++ src/main/java/org/yinwang/rubysonar/ast/Index.java
@@ -27,4 +27,8 @@ public class Index extends Node {
     @NotNull
     @Override
     public String toString() {
-        retu
+        return "(index:" + value + ")";
+    }
+
+
+}
---
+++ src/main/java/org/yinwang/rubysonar/ast/Name.java
@@ -0,0 +1,8 @@
+
+package org.yinwang.rubysonar.ast;
+
+import org.jetbrains.annotations.NotNull;
+import org.yinwang.rubysonar.Analyzer;
+import org.yinwang.rubysonar.Binding;
+import org.yinwang.rubysonar.Constants;
+import org.yinwang.rubysonar.State;
