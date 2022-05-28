9144b2df908a6568f299fb29a3788489b51e9263
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-05-28 11:23:26 +0000

    AAAAAAAAAAARGH!

--- src/main/java/org/yinwang/rubysonar/ast/Node.java
+++ src/main/java/org/yinwang/rubysonar/ast/Node.java
@@ -6,4 +6,8 @@ import org.yinwang.rubysonar.Analyzer;
 import org.yinwang.rubysonar.State;
 import org.yinwang.rubysonar._;
 import org.yinwang.rubysonar.types.Type;
-import org.yinwang.rubysonar.t
+import org.yinwang.rubysonar.types.UnionType;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
