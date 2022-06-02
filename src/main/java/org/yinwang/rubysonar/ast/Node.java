48fd1312a53a05143a1291ae2153f17aaff3b809
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-06-02 08:48:17 +0000

    "Release of v0.2.3-8"

--- src/main/java/org/yinwang/rubysonar/ast/Node.java
+++ src/main/java/org/yinwang/rubysonar/ast/Node.java
@@ -63,4 +63,9 @@ public abstract class Node implements java.io.Serializable {
     public void addChildren(@Nullable Collection<? extends Node> nodes) {
         if (nodes != null) {
             for (Node n : nodes) {
-   
+                if (n != null) {
+                    n.setParent(this);
+                }
+            }
+        }
+    
