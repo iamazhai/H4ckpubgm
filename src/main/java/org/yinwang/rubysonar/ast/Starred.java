b31d62a11049385feb44bc1d4deb848c94f15adc
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-06-16 08:22:55 +0000

    Updated library versions.

--- src/main/java/org/yinwang/rubysonar/ast/Starred.java
+++ src/main/java/org/yinwang/rubysonar/ast/Starred.java
@@ -1,4 +1,10 @@
 package org.yinwang.rubysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
-import org.yinwang.rubysonar.S
+import org.yinwang.rubysonar.State;
+import org.yinwang.rubysonar.types.Type;
+
+
+public class Starred extends Node {
+
+    public
