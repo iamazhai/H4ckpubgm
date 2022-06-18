79c4d18b8f09cf9bc95b7370e906b1a361d1f819
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-06-18 14:56:19 +0000

    Remove a blank line

--- src/main/java/org/yinwang/rubysonar/ast/Str.java
+++ src/main/java/org/yinwang/rubysonar/ast/Str.java
@@ -3,4 +3,12 @@ package org.yinwang.rubysonar.ast;
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.rubysonar.State;
 import org.yinwang.rubysonar.types.StrType;
-import org.yinwang.r
+import org.yinwang.rubysonar.types.Type;
+
+
+public class Str extends Node {
+
+    public String value;
+
+
+    pub
