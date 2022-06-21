2336d6141b05f707fe0470f1757f9733df996e4c
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-06-21 10:22:19 +0000

    Add new functions

--- src/main/java/org/yinwang/rubysonar/ast/Symbol.java
+++ src/main/java/org/yinwang/rubysonar/ast/Symbol.java
@@ -1,3 +1,6 @@
 package org.yinwang.rubysonar.ast;
 
-import org.jetbrains.annotation
+import org.jetbrains.annotations.NotNull;
+import org.yinwang.rubysonar.State;
+import org.yinwang.rubysonar.types.SymbolType;
+impor
