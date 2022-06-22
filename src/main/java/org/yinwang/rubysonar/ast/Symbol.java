fe3f4e8e3f5371fed60158b7997a47c8cf56985b
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-06-22 05:44:58 +0000

    Add .gitconfig

--- src/main/java/org/yinwang/rubysonar/ast/Symbol.java
+++ src/main/java/org/yinwang/rubysonar/ast/Symbol.java
@@ -3,4 +3,10 @@ package org.yinwang.rubysonar.ast;
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.rubysonar.State;
 import org.yinwang.rubysonar.types.SymbolType;
-impor
+import org.yinwang.rubysonar.types.Type;
+
+
+public class Symbol extends Node {
+
+    @NotNull
+    public final St
