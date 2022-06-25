50f1dc05eb47ac0ae98538e94f4ae1ed53326810
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-06-25 06:56:04 +0000

    Update table

--- src/main/java/org/yinwang/rubysonar/ast/Try.java
+++ src/main/java/org/yinwang/rubysonar/ast/Try.java
@@ -3,4 +3,10 @@ package org.yinwang.rubysonar.ast;
 import org.jetbrains.annotations.NotNull;
 import org.yinwang.rubysonar.State;
 import org.yinwang.rubysonar.types.Type;
-import org.yinwang.rubyso
+import org.yinwang.rubysonar.types.UnionType;
+
+
+public class Try extends Node {
+
+    public Node rescue;
+    publi
