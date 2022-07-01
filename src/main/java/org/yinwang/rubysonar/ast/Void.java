d1f9a2fe13ef84af044f1b279216f31b8b44e5c3
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-07-01 20:56:36 +0000

    Update test files

--- src/main/java/org/yinwang/rubysonar/ast/Void.java
+++ src/main/java/org/yinwang/rubysonar/ast/Void.java
@@ -1,4 +1,10 @@
 package org.yinwang.rubysonar.ast;
 
 import org.jetbrains.annotations.NotNull;
-import or
+import org.yinwang.rubysonar.State;
+import org.yinwang.rubysonar.types.Type;
+
+
+public class Void extends Node {
+
+  
