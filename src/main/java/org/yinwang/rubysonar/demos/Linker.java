dcbf952a2206ce824d22291e220b3b056644d76a
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-07-13 13:26:27 +0000

    added skills

--- src/main/java/org/yinwang/rubysonar/demos/Linker.java
+++ src/main/java/org/yinwang/rubysonar/demos/Linker.java
@@ -114,3 +114,128 @@ class Linker {
 
         if (binding.start < 0 || seenDef.contains(hash)) {
             return;
+        }
+
+        seenDef.add(hash);
+        Style style = new Style(Style.Type.ANCHOR, binding.start, binding.end);
+        style.message = binding.type.toString();
+        style.url = binding.qname;
+        style.id = "" + Math.abs(binding.hashCode());
+
+        Set<Node> refs = binding.refs;
+        style.highlight = new ArrayList<>();
+
+
+        for (Node r : refs) {
+            style.highlight.add(Integer.toString(Math.abs(r.hashCode())));
+        }
+        addFileStyle(binding.file, style);
+    }
+
+
+    void processRef(@NotNull Node ref, @NotNull List<Binding> bindings) {
+        String qname = bindings.iterator().next().qname;
+        int hash = ref.hashCode();
+
+        if (!seenRef.contains(hash)) {
+            seenRef.add(hash);
+
+            Style link = new Style(Style.Type.LINK, ref.start, ref.end);
+            link.id = qname;
+
+            List<String> typings = new ArrayList<>();
+            for (Binding b : bindings) {
+                typings.add(b.type.toString());
+            }
+            link.message = _.joinWithSep(typings, " | ", "{", "}");
+
+            // Currently jump to the first binding only. Should change to have a
+            // hover menu or something later.
+            String path = ref.file;
+            if (path != null) {
+                for (Binding b : bindings) {
+                    if (link.url == null) {
+                        link.url = toURL(b, path);
+                    }
+
+                    if (link.url != null) {
+                        addFileStyle(path, link);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    void processRefDebug(@NotNull Node ref, @NotNull List<Binding> bindings) {
+        int hash = ref.hashCode();
+
+        if (!seenRef.contains(hash)) {
+            seenRef.add(hash);
+
+            Style link = new Style(Style.Type.LINK, ref.start, ref.end);
+            link.id = Integer.toString(Math.abs(hash));
+
+            List<String> typings = new ArrayList<>();
+            for (Binding b : bindings) {
+                typings.add(b.type.toString());
+            }
+            link.message = _.joinWithSep(typings, " | ", "{", "}");
+
+            link.highlight = new ArrayList<>();
+            for (Binding b : bindings) {
+                link.highlight.add(Integer.toString(Math.abs(b.hashCode())));
+            }
+
+            // Currently jump to the first binding only. Should change to have a
+            // hover menu or something later.
+            String path = ref.file;
+            if (path != null) {
+                for (Binding b : bindings) {
+                    if (link.url == null) {
+                        link.url = toURL(b, path);
+                    }
+
+                    if (link.url != null) {
+                        addFileStyle(path, link);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Returns the styles (links and extra styles) generated for a given file.
+     *
+     * @param path an absolute source path
+     * @return a possibly-empty list of styles for that path
+     */
+    public List<Style> getStyles(String path) {
+        return stylesForFile(path);
+    }
+
+
+    private List<Style> stylesForFile(String path) {
+        List<Style> styles = fileStyles.get(path);
+        if (styles == null) {
+            styles = new ArrayList<>();
+            fileStyles.put(path, styles);
+        }
+        return styles;
+    }
+
+
+    private void addFileStyle(String path, Style style) {
+        stylesForFile(path).add(style);
+    }
+
+
+    private void processDiagnostic(@NotNull Diagnostic d) {
+        Style style = new Style(Style.Type.WARNING, d.start, d.end);
+        style.message = d.msg;
+        style.url = d.file;
+        addFileStyle(d.file, style);
+    }
