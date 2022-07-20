c8383fa47ef5d2a79fb0bf6aed0346fffa6507a0
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-07-20 15:45:28 +0000

    Create test.c

--- src/main/java/org/yinwang/rubysonar/demos/Style.java
+++ src/main/java/org/yinwang/rubysonar/demos/Style.java
@@ -52,3 +52,11 @@ public class Style implements Comparable<Style> {
 
     public Style(Type type, int start, int end) {
         this.type = type;
+        this.start = start;
+        this.end = end;
+    }
+
+
+    public int end() {
+        return end;
+  
