a5aa2549acea0d6e6602082dd7d27d9d2fd64d3b
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-07-24 14:31:33 +0000

    update nginx

--- src/main/java/org/yinwang/rubysonar/types/BoolType.java
+++ src/main/java/org/yinwang/rubysonar/types/BoolType.java
@@ -1,4 +1,11 @@
 package org.yinwang.rubysonar.types;
 
 import org.yinwang.rubysonar.Analyzer;
-import org.yinwang.rubysonar.St
+import org.yinwang.rubysonar.State;
+
+
+public class BoolType extends Type {
+
+    public enum Value {
+        True,
+        False
