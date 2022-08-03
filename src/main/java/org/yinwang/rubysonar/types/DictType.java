5c8cda3f67ac7582cf1dbd64775b1733ffd554c2
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-08-03 18:33:51 +0000

    update index.rst

--- src/main/java/org/yinwang/rubysonar/types/DictType.java
+++ src/main/java/org/yinwang/rubysonar/types/DictType.java
@@ -6,4 +6,9 @@ import org.jetbrains.annotations.NotNull;
 public class DictType extends Type {
 
     public Type keyType;
-   
+    public Type valueType;
+
+
+    public DictType(Type key0, Type val0) {
+        keyType = key0;
+        valueType = val
