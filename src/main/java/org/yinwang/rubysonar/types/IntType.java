11703e0f7062041b31685525fd61120cf9a9944e
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-08-11 21:58:31 +0000

    stage-local

--- src/main/java/org/yinwang/rubysonar/types/IntType.java
+++ src/main/java/org/yinwang/rubysonar/types/IntType.java
@@ -3,3 +3,7 @@ package org.yinwang.rubysonar.types;
 
 public class IntType extends Type {
 
+    @Override
+    public boolean equals(Object other) {
+        return other instanceof IntType;
+    }
