0d8f596cd4c6501e1f736e99cc9d7f2ad63179a8
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-08-14 20:38:19 +0000

    Dirty commit

--- src/main/java/org/yinwang/rubysonar/types/SymbolType.java
+++ src/main/java/org/yinwang/rubysonar/types/SymbolType.java
@@ -3,4 +3,11 @@ package org.yinwang.rubysonar.types;
 import org.jetbrains.annotations.NotNull;
 
 
-public class SymbolType exten
+public class SymbolType extends Type {
+
+    public String name;
+
+
+    public SymbolType(@NotNull String name) {
+        this.name = name;
+ 
