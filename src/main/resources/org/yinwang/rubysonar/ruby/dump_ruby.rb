ab2a26700952c0ff3de371799af45bc04cd21203
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-08-28 11:56:10 +0000

    More polishing

--- src/main/resources/org/yinwang/rubysonar/ruby/dump_ruby.rb
+++ src/main/resources/org/yinwang/rubysonar/ruby/dump_ruby.rb
@@ -139,3 +139,99 @@ class AstSimplifier
 
   def find_locations(obj)
     def find1(obj)
+      if obj.is_a?(Hash)
+        #if obj[:type] == :binary and not obj[:left]
+        #  puts "problem obj: #{obj.inspect}"
+        #end
+        ret = {}
+        whole_start = nil
+        whole_end = nil
+        start_line = nil
+        end_line = nil
+
+        obj.each do |k, v|
+          if k == :location
+            start_idx = node_start(v)
+            end_idx = ident_end(start_idx)
+            ret[:start] = start_idx
+            ret[:end] = end_idx
+            ret[:start_line] = v[0]
+            ret[:end_line] = v[1]
+            whole_start = start_idx
+            whole_end = end_idx
+            start_line = v[0]
+            end_line = v[1]
+          else
+            new_node, start_idx, end_idx, line_start, line_end = find1(v)
+            ret[k] = new_node
+
+            if start_idx && (!whole_start || whole_start > start_idx)
+              whole_start = start_idx
+              start_line = line_start
+            end
+
+            if end_idx && (!whole_end || whole_end < end_idx)
+              whole_end = end_idx
+              end_line = line_end
+            end
+          end
+        end
+
+        if whole_start
+          # push whole_end to 'end' keyword
+          if [:module, :class, :def, :lambda, :if, :begin, :while, :for]
+              .include?(obj[:type]) and not obj[:mod]
+            locator = whole_end
+            while locator <= @src.length and
+                not 'end'.eql? @src[locator .. locator + 'end'.length-1]
+              locator += 1
+            end
+            if 'end'.eql? @src[locator .. locator + 'end'.length-1]
+              whole_end = locator + 'end'.length
+            end
+          end
+
+          ret[:start] = whole_start
+          ret[:end] = whole_end
+          ret[:start_line] = start_line
+          ret[:end_line] = end_line
+
+          # insert docstrings for node if any
+          if [:module, :class, :def].include?(ret[:type])
+            doc = @docs[start_line]
+            if doc
+              ret[:doc] = doc
+            end
+          end
+        end
+        return ret, whole_start, whole_end, start_line, end_line
+
+      elsif obj.is_a?(Array)
+        ret = []
+        whole_start = nil
+        whole_end = nil
+
+        for v in obj
+          new_node, start_idx, end_idx, line_start, line_end = find1(v)
+          ret.push(new_node)
+          if  start_idx && (!whole_start || whole_start > start_idx)
+            whole_start = start_idx
+            start_line = line_start
+          end
+
+          if end_idx && (!whole_end || whole_end < end_idx)
+            whole_end = end_idx
+            end_line = line_end
+          end
+        end
+
+        return ret, whole_start, whole_end, start_line, end_line
+      else
+        return obj, nil, nil, nil, nil
+      end
+    end
+
+    node, _, _, _, _ = find1(obj)
+    node
+  end
+
