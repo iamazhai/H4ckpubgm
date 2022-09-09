fcb90f007a86abde7a28bf4fe7cf0dae8b5162d0
iamazhai <87902280+iamazhai@users.noreply.github.com>
2022-09-09 08:54:14 +0000

    tcp: implement RFC 5961 4.2

--- tests/class-instance/class-methods.test/classmethods.rb
+++ tests/class-instance/class-methods.test/classmethods.rb
@@ -1,3 +1,16 @@
 # (too) many ways of defining class methods
 
-# ----------------------------
+# -----------------------------------
+class A
+  def self.cm
+    "class method A" 
+  end
+
+  def im
+    "instance method A"
+  end
+
+end
+
+puts A.cm
+puts
